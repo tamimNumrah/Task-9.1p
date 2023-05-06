fbd24635fa48a312609f3d42ab0d002548e85728
tamimNumrah <tamim@numrah.com>
2023-05-07 00:11:00 +1000

    Added all the activity needed

---
+++ app/src/main/java/com/tamim/task71p/CreateAdvertActivity.java
@@ -0,0 +1,14 @@
+package com.tamim.task71p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateAdvertActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_advert);
+    }
+}
--- app/src/main/java/com/tamim/task71p/MainActivity.java
+++ app/src/main/java/com/tamim/task71p/MainActivity.java
@@ -2,13 +2,34 @@ package com.tamim.task71p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button newAdvertButton;
+    Button lostAndFoundButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        newAdvertButton = findViewById(R.id.newAdvertButton);
+        lostAndFoundButton = findViewById(R.id.lostAndFoundButton);
+        newAdvertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,CreateAdvertActivity.class);
+                startActivity(i);
+            }
+        });
+        lostAndFoundButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i= new Intent(MainActivity.this,CreateAdvertActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
